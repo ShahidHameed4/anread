02d6eb92ea1995155fc12ab3e39c6c472e84ae4b
Shahid Hameed <90132007+ShahidHameed4@users.noreply.github.com>
2024-10-27 04:02:07 +0500

    libs, new packages for sections

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import { AppProps } from 'next/app'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import React from "react";
+import { AppProps } from "next/app";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "../styles/index.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/public/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
