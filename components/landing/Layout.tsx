4ed8253dfcc4871df20f1cb9ff7a7e60ccdf6f18
Shahid Hameed <90132007+ShahidHameed4@users.noreply.github.com>
2024-10-27 03:53:55 +0500

    Features Section, base layout and home addition

--- components/landing/Layout.tsx
+++ components/landing/Layout.tsx
@@ -7,15 +7,14 @@ type Props = {
   title?: string;
 };
 
-const Layout = ({ children, title = "This is the default title" }: Props) => (
+const Layout = ({ children, title = "ANREAD - Taking You Ahead" }: Props) => (
   <div className="min-h-screen flex flex-col font-mulish">
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <main className="flex-grow text-teal-700">{children}</main>
-    
+    <main className="flex-grow text-voodoo-700 mt-10">{children}</main>
   </div>
 );
 
